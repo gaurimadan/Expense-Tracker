2e837a25792003d07979db47b516b79f1a14a0af
gaurimadan <gaurimadan12@gmail.com>
2024-05-12 10:47:05 +0530

    backend initiated

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const cors=require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app=express();
-const PORT=process.env.PORT;
+const app = express();
+const PORT = process.env.PORT;
+
+mongoose.connect(
+  "mongodb+srv://gaurimadaan12:abc@cluster0.l4zcspn.mongodb.net/test"
+);
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log("MongoDb connection errror ", error));
+db.once("open", () => console.log("Connected to database"));
+
 app.use(cors());
 app.use(express.json());
+app.listen(PORT, () => {
+  console.log("server is listening to ${PORT}");
+});
