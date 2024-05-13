9dcb607d90c5436a0cc83006d6c658cd513703ab
gaurimadan <gaurimadan12@gmail.com>
2024-05-13 21:21:00 +0530

    Chatbot added

--- frontend/expense_tracker/src/App.js
+++ frontend/expense_tracker/src/App.js
@@ -1,12 +1,14 @@
 import React,{useState} from 'react'
 
 import Navbar from './components/navbar/Navbar';
+import Chatbot from './components/ChatBot/Chatbot';
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Chatbot/>
       
     </div>
   )
